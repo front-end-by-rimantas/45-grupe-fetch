0174a236b1f10660fa23a502d67e6137cd4113c0
Rimantas <belauzas@gmail.com>
2023-06-26 13:38:49 +0300

    Fetch

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 console.log('START');
 
-const response = fetch('http://localhost:4512/data/marks.json')
+function average(list) {
+    let sum = 0;
+
+    for (const item of list) {
+        sum += item;
+    }
+
+    return sum / list.length;
+}
+
+const github = 'https://raw.githubusercontent.com/front-end-by-rimantas/45-grupe-fetch/master/data/marks.json';
+const local = 'http://localhost:4512/data/marks.json';
+const localError = 'http://localhost:4512/data/random.json';
+
+const response = fetch(local)
     .then((res) => res.json())
-    .then((data) => { console.log(data) });
+    .then((data) => {
+        const marksAverage = average(data.marks);
+        console.log('Studento vidurkis:', marksAverage);
+    })
+    .catch(err => console.error('ERROR: kazkas blogai su uzklausa...', err))
+    .finally(() => {
+        console.log('Ir cia paskutinis fetch zingsnis...');
+    });
 
 console.log(response);
 
 console.log('END');
+
